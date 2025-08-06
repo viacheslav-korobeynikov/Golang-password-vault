5d97f2f8f6e697551987d4d88ac81e1ea1b647dc
Viacheslav Korobeynikov <korobeynikovvo@gmail.com>
2025-08-06 17:14:58 +0300

    Два способа создания инстанса struct

--- main.go
+++ main.go
@@ -2,14 +2,39 @@ package main
 
 import "fmt"
 
-func main() {
-	a := [4]int{1, 2, 3, 4}
-	reverse(&a)
-	fmt.Println(a)
+type account struct {
+	login    string
+	password string
+	url      string
 }
 
-func reverse(arr *[4]int) {
-	for index, value := range *arr {
-		(*arr)[len(arr)-1-index] = value
+func main() {
+	login := inputData("Введите логин: ")
+	password := inputData("Введите пароль: ")
+	url := inputData("Введите URL: ")
+
+	account1 := account{
+		login,
+		password,
+		url,
 	}
+	account2 := account{
+		login:    login,
+		password: password,
+		url:      url,
+	}
+
+	outputPassword(login, password, url)
+
+}
+
+func inputData(a string) string {
+	fmt.Println(a)
+	var res string
+	fmt.Scan(&res)
+	return res
+}
+
+func outputPassword(login string, password string, url string) {
+	fmt.Println(login, password, url)
 }
