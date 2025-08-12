87db43ada2fb908ac5dde469c39f8572c0df6b4a
Viacheslav Korobeynikov <korobeynikovvo@gmail.com>
2025-08-12 18:58:44 +0300

    Добавлен альтернативный вариант получения типа данных

--- output/errors.go
+++ output/errors.go
@@ -5,14 +5,28 @@ import (
 )
 
 func PrintError(value any) {
-	switch t := value.(type) {
-	case string:
-		color.Red(t)
-	case int:
-		color.Red("Код ошибки: %d", t)
-	case error:
-		color.Red(t.Error())
-	default:
-		color.Red("Неизвестный тип ошибки")
+	intValue, ok := value.(int)
+	if ok {
+		color.Red("Код ошибки: %d", intValue)
 	}
+	strValue, ok := value.(string)
+	if ok {
+		color.Red(strValue)
+	}
+	errorValue, ok := value.(error)
+	if ok {
+		color.Red(errorValue.Error())
+	}
+	color.Red("Неизвестный тип ошибки")
+
+	//switch t := value.(type) {
+	//case string:
+	//	color.Red(t)
+	//case int:
+	//	color.Red("Код ошибки: %d", t)
+	//case error:
+	//	color.Red(t.Error())
+	//default:
+	//	color.Red("Неизвестный тип ошибки")
+	//}
 }
